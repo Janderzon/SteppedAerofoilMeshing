670c67ef39af4ae1d7786d778c82a6a69914f52c
James Anderson <james.anderson2@btinternet.com>
2021-01-23 17:36:13 +0000

    Created new boundary layer mesh

--- naca0012_mesh_gen.py
+++ naca0012_mesh_gen.py
@@ -1,15 +1,17 @@
 import naca_4_series_points
 import gmsh
 import sys
+import math
 import aerofoil_scale
 
 #Parameters
-n = 25                          #Number of panels for each of the upper and lower surfaces of the aerofoil
+n = 25                          #Number points on the aerofoil surface
+surfaceOrder = 2
 farFieldSize = 10               #Width and height of the far field volume boundary
 farFieldMeshSize = 1            #Target mesh size at the far field volume boundary
 boundaryLayerThickness = 0.2    #Thickness of the boundary layer
 boundaryLayerNumCells = 10      #Number of cells in the boundary layer in the direction normal to the aerofoil surface
-boundaryLayerProgression = 1.5  #Growth rate of the cells in the boundary layer
+boundaryLayerProgression = 1.3  #Growth rate of the cells in the boundary layer
 wakeLength = 6
 wakeStart = 1.5
 wakeEndThickness = 2
@@ -21,7 +23,7 @@ wakeHeightPoints = 20
 ################################################################################
 
 #Get the coordinates of the aerofoil
-coords = naca_4_series_points.points(0, 0, 12, 100, "sin")
+coords = naca_4_series_points.points(0, 0, 12, n*surfaceOrder, "cos")
 
 #Initialize Gmsh and name the model
 gmsh.initialize()
@@ -29,53 +31,80 @@ gmsh.model.add("NACA0012")
 
 #Create the aerofoil points
 aerofoilPoints = []
-trailingEdgePoint = gmsh.model.geo.addPoint(1, 0, 0)
-for i in range(1, len(coords)-1):
+for i in range(0, len(coords)):
     aerofoilPoints.append(gmsh.model.geo.addPoint(coords[i][0], coords[i][1], 0))
 
 #Create the aerofoil lines
-aerofoilSpline = gmsh.model.geo.addSpline(aerofoilPoints)
-upperTrailingEdgeLine = gmsh.model.geo.addLine(trailingEdgePoint, aerofoilPoints[0])
-lowerTrailingEdgeLine = gmsh.model.geo.addLine(aerofoilPoints[-1], trailingEdgePoint)
+aerofoilCurves = []
+for i in range(0, n*2):
+    j = i*surfaceOrder
+    curvePoints = []
+    for k in range(0, surfaceOrder+1):
+        curvePoints.append(aerofoilPoints[k+j])
+    aerofoilCurves.append(gmsh.model.geo.addSpline(curvePoints))
+    gmsh.model.geo.mesh.setTransfiniteCurve(aerofoilCurves[i], 2)
+
+#gmsh.model.geo.mesh.setTransfiniteCurve(aerofoilSpline, n)
+#upperTrailingEdgeLine = gmsh.model.geo.addLine(trailingEdgePoint, aerofoilPoints[0])
+#gmsh.model.geo.mesh.setTransfiniteCurve(upperTrailingEdgeLine, 3)
+#lowerTrailingEdgeLine = gmsh.model.geo.addLine(aerofoilPoints[-1], trailingEdgePoint)
+#gmsh.model.geo.mesh.setTransfiniteCurve(lowerTrailingEdgeLine, 3)
 
 #Create the aerofoil curve loop
-aerofoilLoop = gmsh.model.geo.addCurveLoop([upperTrailingEdgeLine, aerofoilSpline, lowerTrailingEdgeLine])
+#aerofoilLoop = gmsh.model.geo.addCurveLoop([upperTrailingEdgeLine, aerofoilSpline, lowerTrailingEdgeLine])
+
+################################################################################
+#   Boundary Layer Geometry
+################################################################################
 
 #Create boundary layer points
-# leadingEdgeCircStartPoint = gmsh.model.geo.addPoint(0, 0.2, 0)
-# leadingEdgeCircCentrePoint = gmsh.model.geo.addPoint(0, 0, 0)
-# leadingEdgeCircEndPoint = gmsh.model.geo.addPoint(0, -0.2, 0)
-# aboveTrailingEdgePoint = gmsh.model.geo.addPoint(1, 0.2, 0)
-# belowTrailingEdgePoint = gmsh.model.geo.addPoint(1, -0.2, 0)
+boundaryLayerPoints = []
+numStraightPoints = round((n+1)*0.8)
+numCircPoints = (n-round((n+1)*0.8))*2
+for i in range(0, numStraightPoints):
+    j = i*surfaceOrder
+    boundaryLayerPoints.append(gmsh.model.geo.addPoint(coords[j][0], boundaryLayerThickness, 0))
+for i in range(0, numCircPoints+1):
+    angle = i*math.pi/numCircPoints
+    print(angle)
+    boundaryLayerPoints.append(gmsh.model.geo.addPoint(-boundaryLayerThickness*math.sin(angle), boundaryLayerThickness*math.cos(angle), 0))
+for i in range(numCircPoints+numStraightPoints+1, n*2+1):
+    j = i*surfaceOrder
+    boundaryLayerPoints.append(gmsh.model.geo.addPoint(coords[j][0], -boundaryLayerThickness, 0))
 
 #Create the boundary layer lines
-# upperBoundaryLayerLine = gmsh.model.geo.addLine(aboveTrailingEdgePoint, leadingEdgeCircStartPoint)
-# gmsh.model.geo.mesh.setTransfiniteCurve(upperBoundaryLayerLine, 20)
-# lowerBoundaryLayerLine = gmsh.model.geo.addLine(leadingEdgeCircEndPoint, belowTrailingEdgePoint)
-# leadingEdgeCircLine = gmsh.model.geo.addCircleArc(leadingEdgeCircStartPoint, leadingEdgeCircCentrePoint, leadingEdgeCircEndPoint)
-# upperRearBoundaryLayerLine = gmsh.model.geo.addLine(trailingEdgePoint, aboveTrailingEdgePoint)
-# lowerRearBoundaryLayerLine = gmsh.model.geo.addLine(belowTrailingEdgePoint, trailingEdgePoint)
+boundaryLayerLines = []
+for i in range(0, len(boundaryLayerPoints)-1):
+    boundaryLayerLines.append(gmsh.model.geo.addLine(boundaryLayerPoints[i], boundaryLayerPoints[i+1]))
+    gmsh.model.geo.mesh.setTransfiniteCurve(boundaryLayerLines[i], 2)
 
 #Create the boundary layer curve loop
-#boundaryLayerLoop = gmsh.model.geo.addCurveLoop([upperRearBoundaryLayerLine, upperBoundaryLayerLine, leadingEdgeCircLine, lowerBoundaryLayerLine, lowerRearBoundaryLayerLine])
+# boundaryLayerLoop = gmsh.model.geo.addCurveLoop([upperRearBoundaryLayerLine, upperBoundaryLayerLine, leadingEdgeCircLine, lowerBoundaryLayerLine, lowerRearBoundaryLayerLine])
+#gmsh.model.geo.mesh.setTransfiniteCurve(boundaryLayerLoop, n)
+
+#Create boundary layer surface
+#boundaryLayerSurface = gmsh.model.geo.addPlaneSurface([boundaryLayerLoop, aerofoilLoop])
+#gmsh.model.geo.mesh.setTransfiniteSurface(boundaryLayerSurface)
+#gmsh.model.geo.mesh.setRecombine(2,boundaryLayerSurface)
 
 #Create lines to divide up boundary layer into quadrilaterals
-# boundaryLayerDividerLines = []
-# for i in range(0, len(boundaryLayerPoints)):
-#     boundaryLayerDividerLines.append(gmsh.model.geo.addLine(aerofoilPoints[i], boundaryLayerPoints[i]))
-#     gmsh.model.geo.mesh.setTransfiniteCurve(boundaryLayerDividerLines[i],boundaryLayerNumCells,"Progression",boundaryLayerProgression)
+boundaryLayerDividerLines = []
+for i in range(0, len(boundaryLayerPoints)):
+    j = i*surfaceOrder
+    boundaryLayerDividerLines.append(gmsh.model.geo.addLine(aerofoilPoints[j], boundaryLayerPoints[i]))
+    gmsh.model.geo.mesh.setTransfiniteCurve(boundaryLayerDividerLines[i],boundaryLayerNumCells,"Progression",boundaryLayerProgression)
 
 #Create boundary layer quadrilateral curve loops
-# boundaryLayerQuadLoops = []
-# for i in range(0, len(boundaryLayerPoints)):
-#     boundaryLayerQuadLoops.append(gmsh.model.geo.addCurveLoop([boundaryLayerLines[i],-boundaryLayerDividerLines[i],-aerofoilLines[i],boundaryLayerDividerLines[i-1]]))
+boundaryLayerQuadLoops = []
+for i in range(0, len(boundaryLayerPoints)-1):
+    boundaryLayerQuadLoops.append(gmsh.model.geo.addCurveLoop([boundaryLayerLines[i],-boundaryLayerDividerLines[i+1],-aerofoilCurves[i],boundaryLayerDividerLines[i]]))
 
 #Create boundary layer quadrilateral surfaces
-# boundaryLayerQuadSurfaces = []
-# for i in range(0, len(boundaryLayerPoints)):
-#     boundaryLayerQuadSurfaces.append(gmsh.model.geo.addPlaneSurface([boundaryLayerQuadLoops[i]]))
-#     gmsh.model.geo.mesh.setTransfiniteSurface(boundaryLayerQuadSurfaces[i])
-#     gmsh.model.geo.mesh.setRecombine(2,boundaryLayerQuadSurfaces[i])
+boundaryLayerQuadSurfaces = []
+for i in range(0, len(boundaryLayerQuadLoops)):
+    boundaryLayerQuadSurfaces.append(gmsh.model.geo.addPlaneSurface([boundaryLayerQuadLoops[i]]))
+    gmsh.model.geo.mesh.setTransfiniteSurface(boundaryLayerQuadSurfaces[i])
+    gmsh.model.geo.mesh.setRecombine(2,boundaryLayerQuadSurfaces[i])
 
 ################################################################################
 #   Wake Geometry
